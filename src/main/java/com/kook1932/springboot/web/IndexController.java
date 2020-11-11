ff112079301e0a82c9fb656d278578daf008cdb0
kook1932 <gkswjddyd4@gmail.com>
2020-11-11 23:37:01 +0900

    게시글 등록 화면 만들기

--- src/main/java/com/kook1932/springboot/web/IndexController.java
+++ src/main/java/com/kook1932/springboot/web/IndexController.java
@@ -10,6 +10,11 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
 }
 
 // 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정된다.
