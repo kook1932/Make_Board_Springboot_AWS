5f9af350991ed8e2bb8aa863f6b3eaa9ba59598d
kook1932 <gkswjddyd4@gmail.com>
2020-11-11 23:14:25 +0900

    Mustache 설정, 기본 페이지(index.mustache) 만들기

---
+++ src/main/java/com/kook1932/springboot/web/IndexController.java
@@ -0,0 +1,16 @@
+package com.kook1932.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
+
+// 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정된다.
+// prefix + index + mustache -> ViewResolver가 처리
