acf40cff965eefb201587f17f7ff33f194df4e09
kook1932 <gkswjddyd4@gmail.com>
2020-11-11 22:27:06 +0900

    JPA Auditing으로 생성시간/수정시간 자동화하기

--- src/main/java/com/kook1932/springboot/Application.java
+++ src/main/java/com/kook1932/springboot/Application.java
@@ -2,7 +2,9 @@ package com.kook1932.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 // 스프링부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정된다.
 // 이 위치부터 설정을 읽어가기 때문에 이 클래스는 항상 프로젝트 최상단에 위치
--- src/main/java/com/kook1932/springboot/domain/posts/Posts.java
+++ src/main/java/com/kook1932/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.kook1932.springboot.domain.posts;
 
+import com.kook1932.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,7 @@ import javax.persistence.*;
 // 테이블과 링크될 클래스임을 나타낸다
 // 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭한다.
 // SalesManager.java -> sales_manager
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     // 해당 테이블의 PK 필드를 나타낸다.
