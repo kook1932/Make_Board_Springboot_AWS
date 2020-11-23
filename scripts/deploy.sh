4afcb13b67562fd6d441de5d58afb9ddbd55f60c
kook1932 <gkswjddyd4@gmail.com>
2020-11-23 10:35:56 +0900

    배포 자동화 구성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step1
+# step1에서 수동으로 git putll을 통해 직접 빌드했던 부분을 제거
+# Jar를 실행하는 단계에서 몇가지 코드 추가
+
+REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=Make_Board_Springboot_AWS
-
-cd $REPOSITORY/$PROJECT_NAME/
-
-git pull
-
-echo "> 프로젝트 Build 시작"
-
-./gradlew build
-
-echo "> step1 디렉토리로 이동"
-
-cd $REPOSITORY
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
@@ -35,12 +26,16 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name : $JAR_NAME"
 
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
+        -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties, /home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=real \
-        $REPOSITORY/$JAR_NAME 2>&1 &
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
